efdabdf861c3894e16fab62416aa647b36cf2fa6
Jashawns <shawny583@hotmail.com>
2022-01-17 21:36:42 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// navbar
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Jashawn Stewart',
+      headerlinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'},
+      ], 
+      home: {
+        title: 'Be Relentless',
+        subtitle: 'Projects that make a difference',
+        text: 'Checkout my projects below'
+      }, 
+      about: {
+        title: 'About Me'
+      },
+      contact: {
+        title: 'Let\'s Talk'
+      } 
+    }
+  }
 
   render() {
     return (
