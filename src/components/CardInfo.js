f8eabc70ee708c797e76dec0ba63f8d20c3ec90a
Jashawns <shawny583@hotmail.com>
2022-01-26 00:35:59 -0500

    update

---
+++ src/components/CardInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CardInfo(props){
+
+    return( 
+        <p>Card Info!!!</p>
+    );
+
+}
+
+export default CardInfo;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,17 @@
 import React from 'react';
+
+import Card from '../components/Card';
 import bookSearch from '../assets/images/bookSearch.png';
 import bookworm from '../assets/images/bookworm.png';
 import CodeQuiz from '../assets/images/CodeQuiz.png';
+import noteTaker from '../assets/images/noteTaker.png';
+import scheduler from '../assets/images/scheduler.png';
+import team from '../assets/images/team.png';
+import weather from '../assets/images/weather.png';
+import workTracker from '../assets/images/workTracker.png';
+import yoda from '../assets/images/yoda.png';
+import { Container } from 'react-bootstrap';
+
 
 class Carousel extends React.Component {
     constructor(props){
@@ -32,6 +42,54 @@ class Carousel extends React.Component {
                     link: 'https://github.com/Jashawns/codeQuiz', 
                     selected: false
                 },
+                {
+                    id: 3, 
+                    title: 'Dev Grub', 
+                    subTitle: 'The cookbook for developers',
+                    imgSrc: noteTaker, 
+                    link: 'https://github.com/Jashawns/note_taker', 
+                    selected: false
+                },
+                {
+                    id: 4, 
+                    title: 'Garrett Love', 
+                    subTitle: 'YouTube channel',
+                    imgSrc: scheduler, 
+                    link: 'https://github.com/Jashawns/workDayScheduler.git', 
+                    selected: false
+                },
+                {
+                    id: 5, 
+                    title: 'Evverest', 
+                    subTitle: 'A social network for developers',
+                    imgSrc: team, 
+                    link: 'https://github.com/Jashawns/team_profile_generator', 
+                    selected: false
+                },
+                {
+                    id: 6, 
+                    title: 'Evverest', 
+                    subTitle: 'A social network for developers',
+                    imgSrc: weather, 
+                    link: 'https://github.com/Jashawns/weather_dashboard', 
+                    selected: false
+                },
+                {
+                    id: 7, 
+                    title: 'Evverest', 
+                    subTitle: 'A social network for developers',
+                    imgSrc: workTracker, 
+                    link: 'https://github.com/Jashawns/workout_tracker', 
+                    selected: false
+                },
+                {
+                    id: 8, 
+                    title: 'Evverest', 
+                    subTitle: 'A social network for developers',
+                    imgSrc: yoda, 
+                    link: 'https://github.com/caitbopp/yoda-advice', 
+                    selected: false
+                },
             ]
             
 
@@ -54,9 +112,20 @@ class Carousel extends React.Component {
 
     }
 
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return(
-            <p>Carousel Works!</p>
+            <Container fluid={false}>
+                <div class="d-flex justify-content-around">
+                    {this.makeItems(this.state.items)}
+
+                </div>
+            </Container>
         );
 
     }
