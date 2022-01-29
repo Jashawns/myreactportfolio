945e40b098863c0387b80a0068eca0647457e49c
Jashawns <shawny583@hotmail.com>
2022-01-29 13:23:51 -0500

    cant get rid of 403 error

--- server/app.js
+++ server/app.js
@@ -18,4 +18,30 @@ app.get('/', (req, res, next) => {
     res.send('API Status: I\'m Awesome')
 });
 
+app.post('/api/email', (req,res, next) => {
+    sendGrid.setApiKey('SG.FQ3IB-U-Q02QiGvJ7tW-Sg.Okw7V4gdft0SdgkVSOACUg9_uhiS5O-X-VW29WMiH2A');
+    const msg = {
+        to: 'shawny583@hotmail.com',
+        from: req.body.email,
+        subject: 'Website Contact',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+    .then(result => {
+
+        res.status(200).json({
+            sucess: true
+        });
+    })
+
+    .catch(err => {
+        console.log('error:', err);
+        res.status(401).json({
+            success: false
+        });
+
+    });
+});
+
 app.listen(3030, '0.0.0.0');
